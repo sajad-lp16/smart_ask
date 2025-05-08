d045caed74a45f43b6ab273ceb8a8725a7880971
Sajad Tohidi <sajad@avicennaresearch.com>
2025-05-09 00:02:21 +0330

    add gemini client

--- ai/__init__.py
+++ ai/__init__.py
@@ -3,12 +3,18 @@ from llama_index.core import Settings
 from llama_index.llms.openai import OpenAI
 
 from core.config import (
-    DEEPSEEK_MODEL,
-    OPENAI_MODEL,
-    DEEPSEEK_API_KEY,
-    OPENAI_KEY,
-    DEEPSEEK_BASE_URL,
     OPENAI_BASE_URL,
+    OPENAI_MODEL,
+    OPENAI_KEY,
+
+    DEEPSEEK_BASE_URL,
+    DEEPSEEK_MODEL,
+    DEEPSEEK_API_KEY,
+
+    GEMINI_BASE_URL,
+    GEMINI_MODEL,
+    GEMINI_API_KEY,
+
     AI_FOR_RESPONSE
 )
 
@@ -16,17 +22,20 @@ os.environ.setdefault("OPENAI_API_KEY", OPENAI_KEY)
 
 ai_2_model = {
     "deepseek": DEEPSEEK_MODEL,
-    "openai": OPENAI_MODEL
+    "openai": OPENAI_MODEL,
+    "gemini": GEMINI_MODEL
 }
 
 ai_2_apikey = {
     "deepseek": DEEPSEEK_API_KEY,
-    "openai": OPENAI_KEY
+    "openai": OPENAI_KEY,
+    "gemini": GEMINI_API_KEY
 }
 
 ai_2_base_url = {
     "deepseek": DEEPSEEK_BASE_URL,
-    "openai": OPENAI_BASE_URL
+    "openai": OPENAI_BASE_URL,
+    "gemini": GEMINI_BASE_URL
 }
 
 response_model: str = AI_FOR_RESPONSE
