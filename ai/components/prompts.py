bc1eea912638f1b677011943113bb39b32c08560
Sajad Tohidi <sajad@avicennaresearch.com>
2025-05-09 17:18:49 +0330

    hotfix router prompt

--- ai/components/prompts.py
+++ ai/components/prompts.py
@@ -100,6 +100,11 @@ RESPONSE FORMAT:
   "person_ids": [<Extracted person IDs used for response, ensure integer type and they can be mentioned in these formats (person, personID, person_id person id, person#)>],
   "ticket_ids": [<Extracted ticket IDs used for response, ensure integer type and they can be mentioned in these formats (ticket, ticketID, ticket_id ticket id, ticket#)>]
 }
+HERE IS THE CLIENT INPUT:
+
+```
+%s
+```
 """
 
 ZAMMAD_QA_PROMPT = """I need to implement a RAG system to provide answers for users' questions. I'll provide you with a conversation where the client explains one or more problems, and the staff provides solutions. There might be multiple back-and-forth exchanges between the client and the staff before reaching a solution, or there might be internal notes between multiple staff members. You will receive all of these.
