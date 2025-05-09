4616e1e047935ae8f480367d6d50be1da6a17514
Sajad Tohidi <sajad@avicennaresearch.com>
2025-05-10 02:09:44 +0330

    hotfix topic pipeline

--- ai/components/fetching.py
+++ ai/components/fetching.py
@@ -13,7 +13,7 @@ async def fetch_ai_client(prompt: str, client: CustomAsyncOpenAI = None, parse_j
             completion = await _client.chat.completions.create(
                 **client.get_template(prompt)
             )
-            response = completion.choices[0].message.content.strip('```json').repalce("`", "")
+            response = completion.choices[0].message.content.strip('```json').replace("`", "")
             if parse_json:
                 return json.loads(response)
             return response
